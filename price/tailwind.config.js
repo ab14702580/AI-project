684a7ae67c7bdfb148cddf092c4284ebcb8cf2c7
ab14702580 <abdullahfahad14701470@gmail.com>
2024-11-21 19:33:04 +0600

    add price page

--- price/tailwind.config.js
+++ price/tailwind.config.js
@@ -3,11 +3,30 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      fontFamily: {
+        roboto: ["Roboto","sans-serif"],
+      },
       colors: {
         'm-black': '#161313',
+        'orange-m': '#E34D00',
+        'b-gradient': '#1E1C1C',
+        'content-color': '#BCBCBC',
+        'price-color': '#805Af5',
+      },
+      keyframes: {
+        'infinit-animate':{
+          '0%':{transform: 'translateX(-.2%)'},
+          '25%': {transform: 'translateX(-.2%)'},
+          '45%': {transform: 'translateX(-50%)'},
+          '65%': {transform: 'translateX(-50%)'},
+          '85%': {transform: 'translateX(-100%)'},
+          '100%':{transform: 'translateX(-100%)'},
+        },
+      },
+      animation: {
+        'infinit-animate': 'infinit-animate 10s linear infinite'
       },
     },
   },
   plugins: [],
 }
-
