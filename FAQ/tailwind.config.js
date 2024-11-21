75bbe33d4acea09c1eecc1347a0f7b5cad699f6d
ab14702580 <abdullahfahad14701470@gmail.com>
2024-11-21 23:54:41 +0600

    add new feature

---
+++ FAQ/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'black-10': '#161313',
+        'modify-color':'#805AF5',
+      },
+    },
+  },
+  plugins: [],
+}
