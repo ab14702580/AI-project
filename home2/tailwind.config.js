02a598f346e4b96d1c62f75b187a90337cdbb514
ab14702580 <abdullahfahad14701470@gmail.com>
2024-12-01 17:56:16 +0600

    add page home2

--- home2/tailwind.config.js
+++ home2/tailwind.config.js
@@ -13,7 +13,11 @@ module.exports = {
       },
       animation: {
         'text_animate1':'text_animate1 .5s linear 1',
-        'text_animate2': 'text_animate2 .5s linear 1'
+        'text_animate2': 'text_animate2 .5s linear 1',
+        'infinite_animation': 'infinite_animation 4s ease infinite',
+        'infinite_animation2': 'infinite_animation2 3.5s ease infinite',
+        'infinite_circle-animation': 'infinite_circle_animation 4s ease infinite',
+        'infinite_watterflow': 'infinite_watterflow 2s ease infinite',
       },
       keyframes: {
         text_animate1: {
@@ -23,6 +27,25 @@ module.exports = {
         text_animate2: {
           '0%': {transform: 'translateY(-40px)',opacity:'0'},
           '100%': {transform: 'translateY(0)',opacity: '1'}
+        },
+        infinite_animation:{
+          '0%':{transform:'translateY(-40px)'},
+          '50%': {transform: 'translateY(0)'},
+          '100%':{transform: 'translateY(-40px)'}
+        },
+        infinite_animation2:{
+          '0%':{transform:'translateY(-40px)'},
+          '50%': {transform: 'translateY(0)'},
+          '100%':{transform: 'translateY(-40px)'}
+        },
+        infinite_circle_animation:{
+          '0%':{transform: 'scale(1.3) translate(-5%,-10%)'},
+          '50%':{transform: 'scale(1) translate(80px)'},
+          '100%':{transform: 'scale(1.3) translate(-5%,-10%)'}
+        },
+        infinite_watterflow:{
+          '0%':{transform: 'scale(1)', opacity: '1'},
+          '100%':{transform: 'scale(1.3)', opacity: '0'}
         },
       },
     },
