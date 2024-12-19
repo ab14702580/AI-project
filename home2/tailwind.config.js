b245e728886606672bda53bb4c095a4e1f3ee68b
ab14702580 <abdullahfahad14701470@gmail.com>
2024-12-19 09:48:34 +0600

    add home2 page design

--- home2/tailwind.config.js
+++ home2/tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
         'color-b': '#88868680',
         'black-m2': '#222',
         'content': '#BBBABA',
+        'content-color2': '#D1D1D1',
       },
       animation: {
         'text_animate1':'text_animate1 .5s linear 1',
@@ -18,6 +19,8 @@ module.exports = {
         'infinite_animation2': 'infinite_animation2 3.5s ease infinite',
         'infinite_circle-animation': 'infinite_circle_animation 4s ease infinite',
         'infinite_watterflow': 'infinite_watterflow 2s ease infinite',
+        'infinite_transform': 'infinite_transform 15s linear infinite normal',
+        'infinite_transform2': 'infinite_transform2 15s linear infinite'
       },
       keyframes: {
         text_animate1: {
@@ -47,6 +50,20 @@ module.exports = {
           '0%':{transform: 'scale(1)', opacity: '1'},
           '100%':{transform: 'scale(1.3)', opacity: '0'}
         },
+        infinite_transform:{
+          '0%': {transform: 'translateX(0%)'},
+          '5%': {transform: 'translateX(0%)'},
+          '15%':{transform: 'translateX(-34%)'},
+          '30%': {transform: 'translateX(-34%)'},
+          '44%': {transform: 'translateX(-68%)'},
+          '59%': {transform: 'translateX(-68%)'},
+          '75%': {transform: 'translateX(-103.5%)'},
+          '100%': {transform: 'translateX(-103.5%)'}
+        },
+        infinite_transform2:{
+          '0%': {transform: 'translateX(0%)'},
+          '100%':{transform: 'translateX(-100%)'}
+        }
       },
     },
   },
