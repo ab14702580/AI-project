44e4399d9b6450d7da8d48c6c1a71a85301bb59f
ab14702580 <abdullahfahad14701470@gmail.com>
2024-12-31 12:14:02 +0600

    add services-details

---
+++ services-details/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'black-10': '#161313',
+        'modify-color':'#805AF5',
+        'content-color': '#D1D1D1',
+        'modify-black': '#1D1C1C',
+      },
+    },
+  },
+  plugins: [],
+}
